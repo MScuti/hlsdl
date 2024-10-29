c843daa1a26494bd10a701d1f5b38465807ce9b7
badskyer <badskyer@gmail.com>
2024-10-29 14:12:30 +0800

    add retry

--- hlsdl_test.go
+++ hlsdl_test.go
@@ -26,7 +26,7 @@ func TestDescrypt(t *testing.T) {
 }
 
 func TestDownload(t *testing.T) {
-	hlsDl := New("https://cdn.theoplayer.com/video/big_buck_bunny_encrypted/stream-800/index.m3u8", nil, "./download", "", "", 2, false)
+	hlsDl := New("https://h5dfsg.anwangjd1.com/api/app/media/h5/m3u8/sp/ad/ef/q2/lo/e1b620caf03f43f7b3e43209911d4d56.m3u8?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJwdWJsaWMiLCJleHAiOjE3MzI3NzQyODYsImlzc3VlciI6ImNvbS5idXR0ZXJmbHkiLCJzdWIiOiJhc2lnbiIsInVzZXJJZCI6OTU0NDE1OX0.5IM6AZTASHnvFn8-Jk9lz3b48ZuAUsO6ZHiVherLqrI", nil, "./download", "", "", 2, false)
 	filepath, err := hlsDl.Download()
 	if err != nil {
 		t.Fatal(err)
